5f467363d557a6e29b5e520cbb148e3219c1bd64
pratprat <71321959+PratPratPratt@users.noreply.github.com>
2022-06-17 16:36:05 +0400

    Add files via upload

---
+++ the boring game/script.js
@@ -0,0 +1,42 @@
+function chooser() {
+    let x_random=Math.random();
+    let y_random=Math.random();
+
+    let x;
+    let y;
+
+    if (x_random >= 0 || x_random >= 0.1){
+        x=0
+    }
+    if (x_random >= 0.2 || x_random >= 0.3){
+        x=1
+    }
+    if (x_random >= 0.4 || x_random >= 0.5){
+        x=2
+    }
+    if (x_random >= 0.7 || x_random >= 0.6){
+        x=3
+    }
+    if (x_random >= 0.8 || x_random >= 0.9){
+        x=4
+    }
+
+    if (y_random >= 0 || y_random >= 0.1){
+        y=0
+    }
+    if (y_random >= 0.2 || y_random >= 0.3){
+        y=1
+    }
+    if (y_random >= 0.4 || y_random >= 0.5){
+        y=2
+    }
+    if (y_random >= 0.6 || y_random >= 0.7){
+        y=3
+    }
+    if (y_random >= 0.8 || y_random >= 0.9){
+        y=4
+    }
+
+    document.getElementById("ball").style.gridRowStart=x;
+    document.getElementById("ball").style.gridColumnStart=y ;
+}
